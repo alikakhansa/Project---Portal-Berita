90ae077c262affd2af564757ab06944c889c9ee8
yantidefa <55498117+yantidefa@users.noreply.github.com>
2021-02-24 14:41:18 +0700

    Delete WEB UJIKOM PORTAL BERITA/pages/foto/foto1 directory

--- WEB UJIKOM PORTAL BERITA/pages/foto/foto1/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php 
-ob_start();
-include '../../inc/koneksi.php';
-$id = $_GET['id_foto'];
-
-mysqli_query($koneksi, "DELETE FROM foto WHERE id_foto='$id'")or die(mysqli_error($koneksi));
-header('location:../../author/index.php?page=foto1');
- ?>
