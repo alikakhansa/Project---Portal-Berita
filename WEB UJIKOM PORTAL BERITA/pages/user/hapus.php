566bb6dae82beaed29afa04847ddfe50c6a85188
yantidefa <55498117+yantidefa@users.noreply.github.com>
2021-02-24 14:37:43 +0700

    Delete WEB UJIKOM PORTAL BERITA/pages/user directory

--- WEB UJIKOM PORTAL BERITA/pages/user/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php 
-ob_start();
-include '../../inc/koneksi.php';
-$id = $_GET['id_login'];
-
-mysqli_query($koneksi, "DELETE FROM login WHERE id_login='$id'")or die(mysqli_error($koneksi));
-header('location:../../admin/index.php?page=user');
- ?>
